48ea23cecd7b6ec890786943f3781bffb8105939
Matteo Pierangeli <matteo.pierangeli@yoox.com>
2015-06-30 13:02:17 +0200

    Equitable

---
+++ AdvancedCSharp/OperatorOverloadEquality/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace OperatorOverloadEquality
+{
+	[TestFixture]
+    public class Equality
+    {
+		[Test]
+		public void NotSameObject()
+		{
+			var bing1 = new Bing("california.jpg");
+			var bing2 = new Bing("california.jpg");
+
+			Assert.IsFalse(bing1.Equals(bing2));
+		}
+
+		[Test]
+		public void SameObject()
+		{
+			var bing1 = new EquitableBing("california.jpg");
+			var bing2 = new EquitableBing("california.jpg");
+
+			Assert.IsTrue(bing1.Equals(bing2));
+		}
+    }
+
+	public class Bing
+	{
+		private readonly string _image;
+
+		public Bing(string image)
+		{
+			_image = image;
+		}
+
+		public string Image
+		{
+			get { return _image; }
+		}
+	}
+
+	public class EquitableBing : IEquatable<EquitableBing>
+	{
+		private readonly string _image;
+
+		public EquitableBing(string image)
+		{
+			_image = image;
+		}
+
+		public string Image
+		{
+			get { return _image; }
+		}
+
+		public bool Equals(EquitableBing other)
+		{
+			return string.Equals(_image, other._image);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != this.GetType()) return false;
+			return Equals((EquitableBing) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return (_image != null ? _image.GetHashCode() : 0);
+		}
+	}
+}
