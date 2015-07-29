b2910122b1fe62ea83cf054188e8c1bb10a607f7
Valerio Bonfatti <valerio.bonfatti@yoox.com>
2015-07-29 17:56:13 +0200

    Deferred Execution update and Immediate Operators

---
+++ AdvancedCSharp/Linq/UtilityExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    public static class UtilityExtensions
+    {
+        public static IEnumerable<TResult> CustomSelect<TSource, TResult>(this IEnumerable<TSource> enumerable, Func<TSource, TResult> selector)
+        {
+            return enumerable.Select(item =>
+            {
+                var result = selector(item);
+                Console.WriteLine("Processing item: {0} -> {1}", item, result);
+                return result;
+            });
+        }
+    }
+}
