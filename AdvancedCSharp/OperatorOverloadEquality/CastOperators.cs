7674b65fa86362e91fc26e382fe8383949fba33b
Matteo Pierangeli <matteo.pierangeli@yoox.com>
2016-05-12 16:57:49 +0200

    rework on cast operators examples

--- AdvancedCSharp/OperatorOverloadEquality/CastOperators.cs
+++ AdvancedCSharp/OperatorOverloadEquality/CastOperators.cs
@@ -7,52 +7,76 @@ namespace OperatorsOverload
 	public class CastOperators
 	{
 		/*
-		 * In terms of the intrinsic numerical types, an explicit conversion is required when you attempt to store a larger value in a smaller container, as this could result in a loss of data
-		 * Conversely, an implicit conversion happens automatically when you attempt to place a smaller type in a destination type that will not result in a loss of data
+		 * In terms of the intrinsic numerical types, an explicit conversion is required
+		 * when you attempt to store a larger value in a smaller container, as this could result in a loss of data
+		 * Conversely, an implicit conversion happens automatically when you attempt
+		 * to place a smaller type in a destination type that will not result in a loss of data
 		*/
 		[Test]
 		public void NumericalCast()
 		{
 			int a = 123;
-			Console.WriteLine(a);
+			Console.WriteLine("int a: {0}", a);
+
 			double b = a; // Implicit conversion from int to long.
+			Console.WriteLine("double b converted without loss from int a: {0}", b);
 			b += 0.456;
-			Console.WriteLine(b);
+			Console.WriteLine("double b: {0}", b);
+
 			int c = (int)b; // Explicit conversion from long to int.
-			Console.WriteLine(c);
+			Console.WriteLine("int c converted with loss from double b: {0}", c);
 		}
 
-		//Class types may be related by classical inheritance (the “is-a” relationship). In this case, the C# conversion process allows you to cast up and down the class hierarchy.
+		private class Base
+		{
+			public int p1 { get; set; }
+		}
+
+		private class Derived : Base
+		{
+			public int p2 { get; set; }
+
+			public Derived(int p2)
+			{
+				this.p2 = p2;
+				p1 = p2 * 2;
+			}
+		}
+
+		//Class types may be related by classical inheritance (the “is-a” relationship).
+		//In this case, the C# conversion process allows you to cast up and down the class hierarchy.
 		[Test]
 		public void HierarchyCast()
 		{
-			// Implicit cast between derived to base.
 			Base myBaseType;
-			myBaseType = new Derived();
-			// Must explicitly cast to store base reference
-			// in derived type.
-			Derived myDerivedType = (Derived)myBaseType;
+			myBaseType = new Derived(33); // Implicit cast between derived to base.
+			Console.WriteLine("base.p1: {0}", myBaseType.p1);
+			//myBaseType.p2; --> compile error because Base is narrower than Derived
+
+			Derived myDerivedType = (Derived)myBaseType; // Must explicitly cast to store Base reference in Derived type.
+			Console.WriteLine("derivate.p1: {0}", myDerivedType.p1);
+			Console.WriteLine("derivate.p2: {0}", myDerivedType.p2);
 		}
 
-		//However, what if you have two class types in different hierarchies with no common parent (other than System.Object) that require conversions?
+		//However, what if you have two class types in different hierarchies
+		//with no common parent (other than System.Object) that require conversions?
 		[Test]
 		public void ExplicitConversion()
 		{
-			var square = (Square)90;
+			var square = (Square)90; //Implicit conversion from Int32 to Square
 			DrawSquare(square);
 
-			var side = (int)square;
+			var side = (int)square; //Implicit conversion from Square to Int32
 			Console.WriteLine("Side length of square = {0}", side);
-			Console.ReadLine();
 		}
 
 		[Test]
 		public void ImplicitConversion()
 		{
-			var square = new Square();
-			square.Length = 7;
+			var square = new Square(7);
 
-			DrawRectangle(square);
+			Rectangle rectangle = square; //Implicit conversion from Square to Rectangle
+			DrawRectangle(rectangle);
 		}
 
 		private static void DrawSquare(Square square)
@@ -66,17 +90,14 @@ namespace OperatorsOverload
 		}
 	}
 
-	public class Derived : Base
+	public class Square
 	{
-	}
+		public int Length { get; private set; }
 
-	public class Base
-	{
-	}
-
-	public struct Square
-	{
-		public int Length { get; set; }
+		public Square(int length)
+		{
+			Length = length;
+		}
 
 		public override string ToString()
 		{
@@ -85,9 +106,7 @@ namespace OperatorsOverload
 
 		public static explicit operator Square(int sideLength)
 		{
-			var newSq = new Square();
-			newSq.Length = sideLength;
-			return newSq;
+			return new Square(sideLength);
 		}
 
 		public static explicit operator int(Square s)
@@ -96,11 +115,16 @@ namespace OperatorsOverload
 		}
 	}
 
-	public struct Rectangle
+	public class Rectangle
 	{
-		public int Width { get; set; }
-		public int Height { get; set; }
+		public int Width { get; private set; }
+		public int Height { get; private set; }
 
+		public Rectangle(int width, int height)
+		{
+			Width = width;
+			Height = height;
+		}
 		public override string ToString()
 		{
 			return string.Format("[Width = {0}; Height = {1}]", Width, Height);
@@ -108,10 +132,7 @@ namespace OperatorsOverload
 
 		public static implicit operator Rectangle(Square s)
 		{
-			var r = new Rectangle();
-			r.Height = s.Length;
-			r.Width = s.Length * 2;
-			return r;
+			return new Rectangle(s.Length, s.Length * 2);
 		}
 	}
 }
